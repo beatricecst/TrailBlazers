bf4f851c107cb81ef706cac26612f8633f2f3b10
BEATRICE <beatricechen2003@gmail.com>
2023-05-29 21:56:37 +0800

    idk

---
+++ src/screens/CreatePost.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { View } from "react-native";
+import Input from "../components/Input";
+import UploadImage from "../components/UploadImage";
+import { StyleSheet } from "react-native";
+import DropDownMenu from "../components/DropDownMenu";
+
+
+export const CreatePost = () => {
+    return (
+        <View style={styles.container}>
+            <UploadImage />
+            <Input label='Caption' placeholder='eg. Best croissant I ever had in Paris!'/>
+            <Input label='Location' placeholder='Tell others how to get there!' />
+            <DropDownMenu />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 30,
+        padding: 50,
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: '#FFE9E0'
+    }, 
+
+})
